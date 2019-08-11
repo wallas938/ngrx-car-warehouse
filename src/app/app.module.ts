0c3065f8e6f5f06c2d838a2a71b9fa1e90789261
wallas938 <33827350+wallas938@users.noreply.github.com>
2019-08-11 20:01:03 +0200

    Recuperation des valeurs dans 'newCarForm'  OP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+/***** start Angular components start *****/
 import { CarsListComponent } from './smarts/cars-list/cars-list.component';
 import { NewCarComponent } from './smarts/new-car/new-car.component';
 import { EditCarComponent } from './smarts/edit-car/edit-car.component';
 import { CarComponent } from './dumbs/car/car.component';
 import { AddFormComponent } from './dumbs/add-form/add-form.component';
 import { EditFormComponent } from './dumbs/edit-form/edit-form.component';
+/***** end Angular components end *****/
+
 /***** start Angular material components start *****/
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -39,6 +45,8 @@ import {MatNativeDateModule} from '@angular/material';
     AppRoutingModule,
     CoreModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatTableModule,
     MatIconModule,
--- src/app/dumbs/add-form/add-form.component.ts
+++ src/app/dumbs/add-form/add-form.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 import { MatFormField } from '@angular/material/form-field'
 import { MatSelect } from '@angular/material/select'
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'add-form',
   templateUrl: './add-form.component.html',
@@ -10,6 +11,17 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 })
 export class AddFormComponent implements OnInit {
 
+
+  newCarForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    horsePower: new FormControl('', Validators.required),
+    brand: new FormControl('', Validators.required),
+    fuelType: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    startOfSales: new FormControl('', Validators.required),
+    endOfSales: new FormControl('', Validators.required),
+  })
+
   startOfSales: any
   minDate = new Date()
   maxDate = new Date(2019, 11, 30)
@@ -17,6 +29,10 @@ export class AddFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.newCarForm.value)
   }
 
 }
